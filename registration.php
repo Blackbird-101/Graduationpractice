3c1bdc045e0456eaed0e51ab5b611e13f5a22dbe
Blackbird-101 <blackbird.art89@gmail.com>
2025-09-29 18:38:16 +0500

    Обновление registration.php

--- registration.php
+++ registration.php
@@ -1,4 +1,7 @@
 <?
+session_start();
+$user = $_SESSION['user_auth'];
+
 $db_host = "localhost"; // сервер
 $db_user = "f1160035_moscow_vitte"; // имя пользователя
 $db_pass = "HlDea4ki"; // пароль
@@ -29,28 +32,12 @@ while($row = $result->fetch_assoc())
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Регистрация пользователя</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-2 m-0 p-0">
-              <div class="flex-shrink-0 p-3 bg-dark  " style="height: 100vh;"> 
-            <a href="/" class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
-              <img class="mb-4" src="logo-muiv.svg" alt="" width="159" height="65"> 
-              <span class="fs-5 fw-semibold "></span> 
-            </a> 
-             <div class="d-flex justify-center flex-column">
-               <button class="text-white btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true"><img width="24" height="24" src="https://img.icons8.com/fluency/48/create.png" alt="create"/> Создание заданий</button>  
-               <button class="text-white btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true"><img width="24" height="24" src="https://img.icons8.com/fluency/50/list.png" alt="list"/>Список дисциплин</button> 
-               <button class="text-white btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true"><img width="24" height="24" src="https://img.icons8.com/color/48/courses.png" alt="courses"/>Учебные материалы</button> <br><br><br>
-               <a href="./auth.html" class="btn btn-danger d-inline-flex align-items-center rounded collapsed"  aria-expanded="true">Авторизация</a> <br>
-               <a href="" class="btn btn-danger d-inline-flex align-items-center rounded collapsed"  aria-expanded="true">Создание пользователя</a> 
-             </div>
-          </div>
-        </div>
-        <div class="col-md-10 mt-5 mb-5">
+ <? include 'header.php'; ?>
             <div class="container">
                 <h2>Добавление пользователя</h2>
                 <form action="" method="post" class="reg-form" id="regForm">
@@ -58,15 +45,15 @@ while($row = $result->fetch_assoc())
                   <div class="col-md-6">
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Почта</label>
-                        <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <input required type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Имя</label>
-                        <input type="text" name="username" class="form-control" id="exampleFormControlInput1" placeholder="Иван">
+                        <input required type="text" name="username" class="form-control" id="exampleFormControlInput1" placeholder="Иван">
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Фамилия</label>
-                        <input type="text" name="surname" class="form-control" id="exampleFormControlInput1" placeholder="Иванов">
+                        <input required type="text" name="surname" class="form-control" id="exampleFormControlInput1" placeholder="Иванов">
                     </div>
                   </div>
                   <div class="col-md-6">
@@ -80,7 +67,7 @@ while($row = $result->fetch_assoc())
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Пароль</label>
-                        <input type="password" name="password" class="form-control" id="exampleFormControlInput1" placeholder="">
+                        <input required type="password" name="password" class="form-control" id="exampleFormControlInput1" placeholder="">
                     </div>
                   </div>
                   </div>
@@ -112,9 +99,7 @@ while($row = $result->fetch_assoc())
                   </tbody>
                 </table>
             </div>
-        </div>
-      </div>
-    </div>
+
 
 
 
@@ -134,11 +119,22 @@ while($row = $result->fetch_assoc())
             dataType: 'JSON',
             data: $("#regForm").serialize(),
             success: function (response) {
+                $('form[id=regForm]').trigger('reset');
                 console.log(response.responseText);
+                alert('Пользователь успешно добавлен !');
             },
             error: function (response) {
+                $('form[id=regForm]').trigger('reset');
                 console.log(response.responseText);
             }
+        });
+    });
+     $("#logout").click(function (event) {
+        $.ajax({
+            url: '/logout.php',
+            success: function () {
+                document.location.href = '/index.php'
+            },
         });
     });
 </script>  
